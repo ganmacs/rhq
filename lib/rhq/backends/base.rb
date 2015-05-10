4f1db9000412e20dbf22f854596630a9ede43d95
ganmacs <ganmacs@gmail.com>
2015-05-11 08:50:42 +0900

    fix wrong method naem

--- lib/rhq/backends/base.rb
+++ lib/rhq/backends/base.rb
@@ -4,9 +4,9 @@ module Rhq
     class Base
       def clone(remote, local)
         if local.exist?
-          print "destination path(#{local.path}) is already exist.\n"
+          print "destination path(#{local.full_path}) is already exist.\n"
         else
-          result = Runner.new.run(command, remote.path, local.path)
+          result = Runner.new.run(command, remote.full_path, local.url)
           raise result[:stderr] if result[:status] != 0
         end
       end
